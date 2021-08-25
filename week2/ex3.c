a5e83d33e8177b1605fb39aa02ab67bf0a0c1b7f
Artem Batalov <a@batalov.me>
2021-08-25 14:09:08 +0300

    task 5

--- week2/ex3.c
+++ week2/ex3.c
@@ -9,9 +9,32 @@ void print(char c, int qty) {
 int main(int argc, char **argv) {
     int n;
     sscanf(argv[1], "%d", &n);
+
+    printf("Main figure:\n");
     for (int i = 1; i <= n; i++) {
         print(' ', n - i);
         print('*', 2*i - 1);
+        printf("\n");
+    }
+
+    printf("\nFirst additional figure:\n");
+    for (int i = 1; i <= n; i++) {
+        print('*', i);
+        printf("\n");
+    }
+
+    printf("\nSecond additional figure:\n");
+    for (int i = 1; i <= (n+1); i++) {
+        if (i < (n+1)/2 +1)
+            print('*', i);
+        else
+            print('*', (n+1)-i);
+        printf("\n");
+    }
+
+    printf("\nThird additional figure:\n");
+    for (int i = 1; i <= n; i++) {
+        print('*', n);
         printf("\n");
     }
 
